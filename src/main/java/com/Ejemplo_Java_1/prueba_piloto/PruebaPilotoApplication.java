fec3a41d18e0f4452d8d9bd1e0dc48fa009da4b3
lfsands3 <107266545+lfsands3@users.noreply.github.com>
2022-10-31 18:08:32 -0500

    Add files via upload

---
+++ src/main/java/com/Ejemplo_Java_1/prueba_piloto/PruebaPilotoApplication.java
@@ -0,0 +1,13 @@
+package com.Ejemplo_Java_1.prueba_piloto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaPilotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaPilotoApplication.class, args);
+	}
+
+}
